7620717ff7263ccdfbc0105dadcf1eba08e5efb8
chenchunqiu <chenchunqiu@chenchunqiudemacbook-pro.local>
2016-03-13 22:32:12 +0800

    初始化项目

---
+++ CQNeteasNews/CQNeteasNews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CQNeteasNews
+//
+//  Created by chenchunqiu on 16/3/13.
+//  Copyright © 2016年 chenchunqiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CQNeteasNews/CQNeteasNews/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CQNeteasNews
+//
+//  Created by chenchunqiu on 16/3/13.
+//  Copyright © 2016年 chenchunqiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
